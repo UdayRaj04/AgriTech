30cff5cbb977e013d6ff8ba533816f7949779965
Uday Raj Sharma <127880768+UdayRaj04@users.noreply.github.com>
2026-07-12 21:47:59 +0530

    Change CORS middleware to allow all origins

--- backend/main11.py
+++ backend/main11.py
@@ -18,7 +18,7 @@ FRONTEND_URL = "http://localhost:3000"
 # Add CORS middlewareallow_origins=[ FRONTEND_URL , "http://127.0.0.1:3000"],
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ FRONTEND_URL, "http://127.0.0.1:3000" ],
+    allow_origins=[ "*" ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
